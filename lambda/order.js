e9f4e912fa20418395887b9481c8af993acdc467
Ahmad Sadeghi <ahmad.sadeghi@trilogy.com>
2023-10-20 11:41:53 +0300

    Completed PlaceOrder

--- lambda/order.js
+++ lambda/order.js
@@ -1,48 +1,53 @@
 const AWS = require('aws-sdk');
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
-
-const { GraphQLScalarType } = require('graphql');
-const { Kind } = require('graphql/language');
-
-const LocalDateTime = new GraphQLScalarType({
-    name: 'LocalDateTime',
-    description: 'Local date and time in ISO 8601 format',
-    serialize(value) {
-        // Convert Date object to ISO 8601 string
-        return value.toISOString();
-    },
-    parseValue(value) {
-        // Convert ISO 8601 string to Date object
-        return new Date(value);
-    },
-    parseLiteral(ast) {
-        if (ast.kind === Kind.STRING) {
-            // Convert ISO 8601 string to Date object
-            return new Date(ast.value);
-        }
-        return null;
-    },
-});
+const crypto = require("crypto");
 
 exports.handler = async (event) => {
-    const customerEmail = event.identity.username;
-    const { shippingAddress, orderDetails } = event.arguments;
-    const orderDate = new Date().toISOString();
+    console.log("New Event:", event);
+    const customerEmail = event.identity.claims.email;
+    const { input: { Country, City, County, Street, Details } } = event.arguments;
+
+    const orderDate = new Date();
+
+
+    const id = crypto.createHash('sha256').update(customerEmail + Date.now()).digest('hex');
 
     const order = {
-        id: `ORDER#${Date.now()}`, // Generate a unique order ID
-        customerEmail,
-        shippingAddress,
-        orderDetails,
-        orderDate,
+        pk: `ORDER#`,
+        sk: `ORDER#${id}`,
+        Id: id,
+        CustomerEmail: customerEmail,
+        AddressCountry: Country,
+        AddressCity: City,
+        AddressCounty: County,
+        AddressStreet: Street,
+        CreatedAt: orderDate.toISOString(),
+        gs1pk: "ORDERSBYDATE",
+        gs1sk: `ORDERDATE#${orderDate.getDate().toString()}`,
     };
+
+    const orderDetailsItems = Details.map((detail, index) => ({
+        pk: `ORDERDETAIL#`,
+        sk: `ORDERDETAIL#${id}#${index}`,
+        OrderId: id,
+        ProductId: detail.ProductId,
+        Quantity: detail.Quantity,
+        gs1pk: `ORDER#${id}`,
+        gs1sk: `DETAIL#${id}#${index}`,
+    }));
 
     const params = {
-        TableName: process.env.SINGLE_TABLE_NAME,
-        Item: order,
+        RequestItems: {
+            [process.env.SINGLE_TABLE_NAME]: [
+                { PutRequest: { Item: order } },
+                ...orderDetailsItems.map(detail => ({ PutRequest: { Item: detail } })),
+            ],
+        },
     };
 
-    await dynamoDB.put(params).promise();
+    await dynamoDB.batchWrite(params).promise();
+
+    console.log(order);
 
     return order;
 };
